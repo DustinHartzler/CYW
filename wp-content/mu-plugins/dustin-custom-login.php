c73ed145e117845efddffa314af4ba74f4663bfd
Dustin Hartzler <Dustin@HartzlerDM.com>
2015-06-29 16:16:30 -0400

    Custom Login Page
    Moved plugin from Plugins to MU-Plugins

--- wp-content/mu-plugins/dustin-custom-login.php
+++ wp-content/mu-plugins/dustin-custom-login.php
@@ -1,6 +1,6 @@
 <?php
 /* 
-Plugin Name: Scrivener's Custom Login
+Plugin Name: Dustin's Custom Login
 Plugin URI: http://YourWebsiteEngineer.com
 Description: Let's you display a custom login screen
 Version: 1.0
@@ -9,11 +9,11 @@ Author URI: http://YourWebsiteEngineer.com
 */
 
 function custom_login_css(){
-	echo '<link rel="stylesheet" type="text/css" href="' . plugins_url( '/style.css' , __FILE__ ) . '"/> ';
+	echo '<link rel="stylesheet" type="text/css" href="' . plugins_url( 'dustin-custom-login/style.css' , __FILE__ ) . '"/> ';
 }
 add_action ('login_head','custom_login_css');
 
 add_filter('login_headerurl', 'custom_login_header_url');
 function custom_login_header_url($url){
-	return 'http://learnscrivenerfast.com';
+	return 'http://conqueryourwebsite.com';
 }
