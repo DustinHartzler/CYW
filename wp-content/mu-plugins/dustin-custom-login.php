fd0760427eab658cdc9d9a2cae7f61e06fa293ef
Dustin Hartzler <Dustin@HartzlerDM.com>
2015-07-01 15:19:51 -0400

    Updated Login Page

--- wp-content/mu-plugins/dustin-custom-login.php
+++ wp-content/mu-plugins/dustin-custom-login.php
@@ -8,12 +8,26 @@ Author: Dustin Hartzler
 Author URI: http://YourWebsiteEngineer.com
 */
 
-function custom_login_css(){
+function custom_login_css() {
 	echo '<link rel="stylesheet" type="text/css" href="' . plugins_url( 'dustin-custom-login/style.css' , __FILE__ ) . '"/> ';
 }
 add_action ('login_head','custom_login_css');
 
-add_filter('login_headerurl', 'custom_login_header_url');
-function custom_login_header_url($url){
+function custom_login_header_url($url) {
 	return 'http://conqueryourwebsite.com';
+}
+add_filter('login_headerurl', 'custom_login_header_url');
+
+function login_error_override() {
+	return 'Nope! Try again. Incorrect login details.';
+}
+add_filter('login_errors', 'login_error_override');
+
+function login_checked_remember_me() {
+	add_filter( 'login_footer', 'rememberme_checked' );
+}
+add_action( 'init', 'login_checked_remember_me' );
+
+function rememberme_checked() {
+	echo "<script>document.getElementById('rememberme').checked = true;</script>";
 }
